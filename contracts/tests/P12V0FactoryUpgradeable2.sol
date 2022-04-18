c98deb8caeb3f330d012a71d3144d9f3036f1e5e
Jerry <jianyue.wang@p12.dev>
2022-04-18 22:18:44 +0800

    🧪 test: mv sol to test

--- contracts/tests/P12V0FactoryUpgradeable2.sol
+++ contracts/tests/P12V0FactoryUpgradeable2.sol
@@ -1,14 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import './P12V0FactoryUpgradeable.sol';
-import './interfaces/IUniswapV2Router02.sol';
-import './interfaces/IP12V0Factory.sol';
-import './interfaces/IUniswapV2Pair.sol';
-import './interfaces/IUniswapV2Factory.sol';
+import '../factory/P12V0FactoryUpgradeable.sol';
 import '../libraries/FullMath.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import './P12V0ERC20.sol';
 
 // new contract for test
 contract P12V0FactoryUpgradeable2 is P12V0FactoryUpgradeable {
