78c54a98aadac0850b3bf37b8da6771b9d9e1c87
Jerry <jianyue.wang@p12.dev>
2022-04-19 21:58:23 +0800

    🌈 style: format

--- contracts/factory/P12V0FactoryUpgradeable.sol
+++ contracts/factory/P12V0FactoryUpgradeable.sol
@@ -9,7 +9,7 @@ import '../libraries/FullMath.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import './P12V0ERC20.sol';
 
-import "./interfaces/IP12Mine.sol";
+import './interfaces/IP12Mine.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
