60a001ae541c60c5f0a0bae62ac4a647b9774756
Jerry <jianyue.wang@p12.dev>
2022-05-06 21:22:37 +0800

    🐞 fix(ss): wrong file path

--- contracts/secretShop/SecretShopUpgradable.sol
+++ contracts/secretShop/SecretShopUpgradable.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
-import './interface/IDelegate.sol';
-import './interface/IWETHUpgradable.sol';
+import './interfaces/IDelegate.sol';
+import './interfaces/IWETHUpgradable.sol';
 
-import './interface/ISecretShopUpgradable.sol';
+import './interfaces/ISecretShopUpgradable.sol';
 import './MarketConsts.sol';
 import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
