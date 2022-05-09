886d2c3fcce3c2aca717315ae44d119fc3f6d44a
Jerry <jianyue.wang@p12.dev>
2022-05-09 11:43:26 +0800

    🐳 chore: use solidify 0.8.13

--- contracts/staking/P12RewardVault.sol
+++ contracts/staking/P12RewardVault.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.13;
 pragma experimental ABIEncoderV2;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
--- contracts/tests/GameCoin.sol
+++ contracts/tests/GameCoin.sol
@@ -1,7 +1,7 @@
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.13;
 
 contract GameCoin is ERC20, Ownable {
   constructor(
