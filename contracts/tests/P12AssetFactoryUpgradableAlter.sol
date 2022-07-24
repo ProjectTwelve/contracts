61693940f3c47cb373a26e9cc60f6823a2c1dfaa
Jerry <jianyue.wang@p12.dev>
2022-07-24 07:23:51 +0000

    🦄 refactor: rename sft-factory to sftFactory

--- contracts/tests/P12AssetFactoryUpgradableAlter.sol
+++ contracts/tests/P12AssetFactoryUpgradableAlter.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.15;
 
-import '../sft-factory/P12AssetFactoryUpgradable.sol';
+import '../sftFactory/P12AssetFactoryUpgradable.sol';
 
 contract P12AssetFactoryUpgradableAlter is P12AssetFactoryUpgradable {
   function setP12factory(address newAddr) public onlyOwner {
