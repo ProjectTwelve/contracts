f92daf6ca2a1652ff8f9e2407c312f8a90395451
Jerry <jianyue.wang@p12.dev>
2022-08-07 16:03:55 +0000

    🐞 fix: pass type check

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 import { HardhatUserConfig, task } from 'hardhat/config';
 import { addFlatTask } from './flat';
 import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
 import 'hardhat-deploy';
