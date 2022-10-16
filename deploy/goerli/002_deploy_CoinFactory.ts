cedb020e233c682dd3c52f05b07af26b51eede66
River <754865974@qq.com>
2022-10-16 21:02:27 +0800

    🐳 chore(deploy): deploy goerli info

---
+++ deploy/goerli/002_deploy_CoinFactory.ts
@@ -0,0 +1,31 @@
+import { DeployFunction } from 'hardhat-deploy/types';
+import { ethers } from 'hardhat';
+
+const func: DeployFunction = async function ({ deployments, getNamedAccounts }) {
+  const { get, deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const p12Token = await get('P12Token');
+  const uniswapFactory = await get('UniswapV2Factory');
+  const uniswapRouter = await get('UniswapV2Router02');
+
+  // Be carefully: Check whether proxy contract is initialized successfully
+  await deploy('P12CoinFactoryUpgradeable', {
+    from: deployer,
+    args: [],
+    proxy: {
+      proxyContract: 'ERC1967Proxy',
+      proxyArgs: ['{implementation}', '{data}'],
+      execute: {
+        init: {
+          methodName: 'initialize',
+          args: [p12Token.address, uniswapFactory.address, uniswapRouter.address, 100, ethers.utils.randomBytes(32)],
+        },
+      },
+    },
+    log: true,
+  });
+};
+func.tags = ['P12CoinFactory'];
+
+export default func;
---
+++ deploy/goerli/007_deploy_Mine.ts
@@ -0,0 +1,37 @@
+import { DeployFunction } from 'hardhat-deploy/types';
+import { ethers } from 'hardhat';
+
+const func: DeployFunction = async function ({ deployments, getNamedAccounts }) {
+  const { deploy, get } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  const p12 = await get('P12Token');
+  const p12CoinFactoryUpgradeable = await get('P12CoinFactoryUpgradeable');
+  const gaugeControllerUpgradeable = await get('GaugeControllerUpgradeable');
+
+  await deploy('P12MineUpgradeable', {
+    from: deployer,
+    args: [],
+    proxy: {
+      proxyContract: 'ERC1967Proxy',
+      proxyArgs: ['{implementation}', '{data}'],
+      execute: {
+        init: {
+          methodName: 'initialize',
+          args: [
+            p12.address,
+            p12CoinFactoryUpgradeable.address,
+            gaugeControllerUpgradeable.address,
+            60,
+            60,
+            ethers.BigNumber.from(5n * 10n ** 17n),
+          ],
+        },
+      },
+    },
+    log: true,
+  });
+};
+func.tags = ['Mine'];
+export default func;
