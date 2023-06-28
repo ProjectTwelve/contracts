a2ab9d7caaeb6b1ea3a2d7ca624accc125df8421
Noy <59097122+noyyyy@users.noreply.github.com>
2023-06-28 15:22:08 +0800

    🧪 test: fix forge build

--- test/coinFactory/CoinFactoryUpgradeable.t.sol
+++ test/coinFactory/CoinFactoryUpgradeable.t.sol
@@ -9,132 +9,127 @@ import 'src/staking/interfaces/IGaugeController.sol';
 import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 import 'src/coinFactory/interfaces/IP12GameCoin.sol';
 import 'src/token/P12Token.sol';
-contract CoinFactoryUpgradeableTest is Test{
+
+contract CoinFactoryUpgradeableTest is Test {
   P12CoinFactoryUpgradeable public p12CoinFactoryUpgradeable;
   P12Token public p12Token;
-  
-  function setUp()public{
+  address _owner = vm.addr(1);
+
+  function setUp() public {
     p12CoinFactoryUpgradeable = new P12CoinFactoryUpgradeable();
-    p12Token = new P12Token('Project Twelve', 'P12', 10000*1e18);
+    p12Token = new P12Token(_owner, 'Project Twelve', 'P12', 10000 * 1e18);
   }
 
-  function testSetDev(address newDev)public{
+  function testSetDev(address newDev) public {
     vm.prank(p12CoinFactoryUpgradeable.owner());
     p12CoinFactoryUpgradeable.setDev(newDev);
-    assertEq(p12CoinFactoryUpgradeable.dev(),newDev);
+    assertEq(p12CoinFactoryUpgradeable.dev(), newDev);
   }
 
-  function testSetP12Mine(IP12MineUpgradeable newP12Mine)public{
+  function testSetP12Mine(IP12MineUpgradeable newP12Mine) public {
     vm.prank(p12CoinFactoryUpgradeable.owner());
     p12CoinFactoryUpgradeable.setP12Mine(newP12Mine);
-    assertEq(p12CoinFactoryUpgradeable.p12Mine.address,address(newP12Mine));
+    assertEq(p12CoinFactoryUpgradeable.p12Mine.address, address(newP12Mine));
   }
 
-  function testSetGaugeController(IGaugeController newGaugeController)public{
+  function testSetGaugeController(IGaugeController newGaugeController) public {
     vm.prank(p12CoinFactoryUpgradeable.owner());
     p12CoinFactoryUpgradeable.setGaugeController(newGaugeController);
-    assertEq(p12CoinFactoryUpgradeable.gaugeController.address,address(newGaugeController));
+    assertEq(p12CoinFactoryUpgradeable.gaugeController.address, address(newGaugeController));
   }
 
-
-  function testSetP12Token(address NewP12Token)public{
+  function testSetP12Token(address NewP12Token) public {
     vm.prank(p12CoinFactoryUpgradeable.owner());
     p12CoinFactoryUpgradeable.setP12Token(NewP12Token);
-    assertEq(p12CoinFactoryUpgradeable.p12(),NewP12Token);
+    assertEq(p12CoinFactoryUpgradeable.p12(), NewP12Token);
   }
 
-  function testSetUniswapFactory(IUniswapV2Factory NewUniswapFactory)public{
+  function testSetUniswapFactory(IUniswapV2Factory NewUniswapFactory) public {
     vm.prank(p12CoinFactoryUpgradeable.owner());
     p12CoinFactoryUpgradeable.setUniswapFactory(NewUniswapFactory);
-    assertEq(p12CoinFactoryUpgradeable.uniswapFactory.address,address(NewUniswapFactory));
+    assertEq(p12CoinFactoryUpgradeable.uniswapFactory.address, address(NewUniswapFactory));
   }
 
-
-  function testSetUniswapRouter(IUniswapV2Router02 newUniswapRouter)public{
+  function testSetUniswapRouter(IUniswapV2Router02 newUniswapRouter) public {
     vm.prank(p12CoinFactoryUpgradeable.owner());
     p12CoinFactoryUpgradeable.setUniswapRouter(newUniswapRouter);
-    assertEq(p12CoinFactoryUpgradeable.uniswapRouter.address,address(newUniswapRouter));
+    assertEq(p12CoinFactoryUpgradeable.uniswapRouter.address, address(newUniswapRouter));
   }
 
-
-  function testRegister(string memory gameId, address developer)public{
+  function testRegister(string memory gameId, address developer) public {
     vm.prank(p12CoinFactoryUpgradeable.dev());
-    p12CoinFactoryUpgradeable.register(gameId,developer);
-    assertEq(p12CoinFactoryUpgradeable.allGames(gameId),developer);
+    p12CoinFactoryUpgradeable.register(gameId, developer);
+    assertEq(p12CoinFactoryUpgradeable.allGames(gameId), developer);
   }
 
-
-  function testCreate(string memory name,
+  function testCreate(
+    string memory name,
     string memory symbol,
     string memory gameId,
     string memory gameCoinIconUrl,
     uint96 amountGameCoin,
-    uint96 amountP12)public{
-      vm.prank(p12Token.owner());
-      p12Token.mint(p12CoinFactoryUpgradeable.allGames(gameId), amountP12);
-      p12Token.approve(address(p12CoinFactoryUpgradeable),amountP12);
-      vm.prank(p12CoinFactoryUpgradeable.allGames(gameId));
-      IP12GameCoin gameCoin = p12CoinFactoryUpgradeable.create(name,symbol, gameId, gameCoinIconUrl, amountGameCoin,amountP12);
-      assertEq(gameCoin.balanceOf(address(p12CoinFactoryUpgradeable)),amountGameCoin/2);
-    }
-
-
-  function testQueueMintCoin(string memory gameId,
-    IP12GameCoin gameCoinAddress,
-    uint256 amountGameCoin)public{
+    uint96 amountP12
+  ) public {
+    vm.prank(p12Token.owner());
+    p12Token.mint(p12CoinFactoryUpgradeable.allGames(gameId), amountP12);
+    p12Token.approve(address(p12CoinFactoryUpgradeable), amountP12);
     vm.prank(p12CoinFactoryUpgradeable.allGames(gameId));
-    bool status = p12CoinFactoryUpgradeable.queueMintCoin(gameId,gameCoinAddress, amountGameCoin);
+    IP12GameCoin gameCoin = p12CoinFactoryUpgradeable.create(name, symbol, gameId, gameCoinIconUrl, amountGameCoin, amountP12);
+    assertEq(gameCoin.balanceOf(address(p12CoinFactoryUpgradeable)), amountGameCoin / 2);
+  }
+
+  function testQueueMintCoin(string memory gameId, IP12GameCoin gameCoinAddress, uint256 amountGameCoin) public {
+    vm.prank(p12CoinFactoryUpgradeable.allGames(gameId));
+    bool status = p12CoinFactoryUpgradeable.queueMintCoin(gameId, gameCoinAddress, amountGameCoin);
     assertTrue(status);
   }
 
-  function testExecuteMintCoin(IP12GameCoin gameCoinAddress, bytes32 mintId)public{
+  function testExecuteMintCoin(IP12GameCoin gameCoinAddress, bytes32 mintId) public {
     uint256 oldBalance = gameCoinAddress.balanceOf(address(p12CoinFactoryUpgradeable));
     bool status = p12CoinFactoryUpgradeable.executeMintCoin(gameCoinAddress, mintId);
     assertTrue(status);
     uint256 Balance = gameCoinAddress.balanceOf(address(p12CoinFactoryUpgradeable));
-    assertGt(Balance,oldBalance);
+    assertGt(Balance, oldBalance);
   }
 
-  function testWithdraw(address userAddress,
-    IP12GameCoin gameCoinAddress,
-    uint256 amountGameCoin)public{
-      vm.prank(p12CoinFactoryUpgradeable.dev());
-      p12CoinFactoryUpgradeable.withdraw(userAddress,gameCoinAddress, amountGameCoin);
-      uint256 balanceOf = gameCoinAddress.balanceOf(userAddress);
-      assertEq(amountGameCoin,balanceOf);
+  function testWithdraw(address userAddress, IP12GameCoin gameCoinAddress, uint256 amountGameCoin) public {
+    vm.prank(p12CoinFactoryUpgradeable.dev());
+    p12CoinFactoryUpgradeable.withdraw(userAddress, gameCoinAddress, amountGameCoin);
+    uint256 balanceOf = gameCoinAddress.balanceOf(userAddress);
+    assertEq(amountGameCoin, balanceOf);
   }
 
-  function testPause()public{
+  function testPause() public {
     vm.prank(p12CoinFactoryUpgradeable.owner());
     p12CoinFactoryUpgradeable.pause();
-
   }
 
-  function testUnpause()public{
+  function testUnpause() public {
     vm.prank(p12CoinFactoryUpgradeable.owner());
     p12CoinFactoryUpgradeable.unpause();
   }
 
-  function testSetDelayK(uint256 newDelayK)public{
+  function testSetDelayK(uint256 newDelayK) public {
     vm.prank(p12CoinFactoryUpgradeable.owner());
     p12CoinFactoryUpgradeable.setDelayK(newDelayK);
-    assertEq(p12CoinFactoryUpgradeable.delayK(),newDelayK);
+    assertEq(p12CoinFactoryUpgradeable.delayK(), newDelayK);
   }
 
-  function testSetDelayB(uint256 newDelayB)public{
+  function testSetDelayB(uint256 newDelayB) public {
     vm.prank(p12CoinFactoryUpgradeable.owner());
     p12CoinFactoryUpgradeable.setDelayB(newDelayB);
-    assertEq(p12CoinFactoryUpgradeable.delayB(),newDelayB);
+    assertEq(p12CoinFactoryUpgradeable.delayB(), newDelayB);
   }
 
   // function testGetMintFee(IP12GameCoin gameCoinAddress, uint256 amountGameCoin)public{
   //   p12CoinFactoryUpgradeable.getMintFee(gameCoinAddress, amountGameCoin);
   // }
 
-
-  function testGetMintDelay(IP12GameCoin gameCoinAddress, uint256 amountGameCoin)public{
-   uint256 timeA = p12CoinFactoryUpgradeable.getMintDelay(gameCoinAddress, amountGameCoin);
-   uint256 timeB = (amountGameCoin * p12CoinFactoryUpgradeable.delayK()) / (IP12GameCoin(gameCoinAddress).totalSupply()) + p12CoinFactoryUpgradeable.delayB();
-   assertEq(timeA,timeB);
+  function testGetMintDelay(IP12GameCoin gameCoinAddress, uint256 amountGameCoin) public {
+    uint256 timeA = p12CoinFactoryUpgradeable.getMintDelay(gameCoinAddress, amountGameCoin);
+    uint256 timeB = (amountGameCoin * p12CoinFactoryUpgradeable.delayK()) /
+      (IP12GameCoin(gameCoinAddress).totalSupply()) +
+      p12CoinFactoryUpgradeable.delayB();
+    assertEq(timeA, timeB);
   }
 }
--- test/staking/P12MineUpgradeable.t.sol
+++ test/staking/P12MineUpgradeable.t.sol
@@ -5,94 +5,108 @@ import 'src/staking/interfaces/IGaugeController.sol';
 import 'src/staking/P12MineUpgradeable.sol';
 import 'src/token/P12Token.sol';
 import 'src/staking/P12MineStorage.sol';
-contract P12MineUpgradeableTest is Test{
+
+contract P12MineUpgradeableTest is Test {
   P12MineUpgradeable public p12MineUpgradeable;
   P12Token public p12Token;
+  address _owner = vm.addr(1);
+
   function setUp() public {
     p12MineUpgradeable = new P12MineUpgradeable();
-    p12Token = new P12Token('Project Twelve', 'P12', 10000*1e18);
+    p12Token = new P12Token(_owner, 'Project Twelve', 'P12', 10000 * 1e18);
   }
 
-  function testSetP12CoinFactory(address newP12CoinFactory)public{
+  function testSetP12CoinFactory(address newP12CoinFactory) public {
     vm.prank(p12MineUpgradeable.owner());
     p12MineUpgradeable.setP12CoinFactory(newP12CoinFactory);
-    assertEq(p12MineUpgradeable.p12CoinFactory.address,newP12CoinFactory);
+    assertEq(p12MineUpgradeable.p12CoinFactory.address, newP12CoinFactory);
   }
 
-  function testSetGaugeController(IGaugeController newGaugeController)public{
-    vm.prank(p12MineUpgradeable.owner());  
+  function testSetGaugeController(IGaugeController newGaugeController) public {
+    vm.prank(p12MineUpgradeable.owner());
     p12MineUpgradeable.setGaugeController(newGaugeController);
-    assertEq(p12MineUpgradeable.gaugeController.address,address(newGaugeController));
+    assertEq(p12MineUpgradeable.gaugeController.address, address(newGaugeController));
+  }
 
-  }
-  function testPoolLength()public{
+  function testPoolLength() public {
     uint len = p12MineUpgradeable.poolLength();
-   // assertEq(p12MineUpgradeable.poolInfos.length,len);
+    // assertEq(p12MineUpgradeable.poolInfos.length,len);
   }
-  function testWithdrawEmergency()public{
-    vm.prank(p12MineUpgradeable.owner()); 
+
+  function testWithdrawEmergency() public {
+    vm.prank(p12MineUpgradeable.owner());
     p12MineUpgradeable.emergency();
     p12MineUpgradeable.withdrawEmergency();
   }
- 
+
   //function testGetWithdrawUnlockTimestamp(address lpToken, uint256 amount)public{}
-  function testGetPid(address lpToken)public{
+  function testGetPid(address lpToken) public {
     uint256 pid = p12MineUpgradeable.getPid(lpToken);
-    assertEq(p12MineUpgradeable.lpTokenRegistry(lpToken)-1,pid);
+    assertEq(p12MineUpgradeable.lpTokenRegistry(lpToken) - 1, pid);
   }
-  function testGetUserLpBalance(address lpToken, address user)public{
+
+  function testGetUserLpBalance(address lpToken, address user) public {
     uint256 pid = p12MineUpgradeable.getPid(lpToken);
-    uint balance  = p12MineUpgradeable.getUserLpBalance(lpToken,user);
+    uint balance = p12MineUpgradeable.getUserLpBalance(lpToken, user);
     //assertEq(p12MineUpgradeable.userInfo(pid,user).amount,balance);
   }
-  function testAddLpTokenInfoForGameCreator(address lpToken,uint256 amount,address gameCoinCreator)public{
-    vm.prank(p12MineUpgradeable.p12CoinFactory()); 
-    p12MineUpgradeable.addLpTokenInfoForGameCreator(lpToken,amount,gameCoinCreator);
+
+  function testAddLpTokenInfoForGameCreator(address lpToken, uint256 amount, address gameCoinCreator) public {
+    vm.prank(p12MineUpgradeable.p12CoinFactory());
+    p12MineUpgradeable.addLpTokenInfoForGameCreator(lpToken, amount, gameCoinCreator);
     //assertEq(p12MineUpgradeable.)
   }
-  function testEmergency()public{
-    vm.prank(p12MineUpgradeable.owner()); 
+
+  function testEmergency() public {
+    vm.prank(p12MineUpgradeable.owner());
     p12MineUpgradeable.emergency();
     assertTrue(p12MineUpgradeable.isEmergency());
   }
-  function testCreatePool(address lpToken)public{
+
+  function testCreatePool(address lpToken) public {
     vm.prank(p12MineUpgradeable.owner());
     p12MineUpgradeable.createPool(lpToken);
     p12MineUpgradeable.createPool(lpToken);
-   // assertEq(p12MineUpgradeable.lpTokenRegistry(lpToken),p12MineUpgradeable.poolInfos.length);
+    // assertEq(p12MineUpgradeable.lpTokenRegistry(lpToken),p12MineUpgradeable.poolInfos.length);
   }
-  function testSetDelayK(uint256 newDelayK)public{
+
+  function testSetDelayK(uint256 newDelayK) public {
     vm.prank(p12MineUpgradeable.owner());
     p12MineUpgradeable.setDelayK(newDelayK);
-    assertEq(p12MineUpgradeable.delayK(),newDelayK);
+    assertEq(p12MineUpgradeable.delayK(), newDelayK);
   }
-  function testSetDelayB(uint256 newDelayB)public{
+
+  function testSetDelayB(uint256 newDelayB) public {
     vm.prank(p12MineUpgradeable.owner());
     p12MineUpgradeable.setDelayB(newDelayB);
-    assertEq(p12MineUpgradeable.delayB(),newDelayB);
+    assertEq(p12MineUpgradeable.delayB(), newDelayB);
   }
-  function testSetRate(uint256 newRate)public{
+
+  function testSetRate(uint256 newRate) public {
     vm.prank(p12MineUpgradeable.owner());
     p12MineUpgradeable.setRate(newRate);
-    assertEq(p12MineUpgradeable.rate(),newRate);
+    assertEq(p12MineUpgradeable.rate(), newRate);
   }
-  
+
   // function testDeposit(address lpToken, uint256 amount)public{
 
   // }
-  function testQueueWithdraw(address lpToken, uint256 amount)public{
-    p12MineUpgradeable.queueWithdraw(lpToken,amount);
+  function testQueueWithdraw(address lpToken, uint256 amount) public {
+    p12MineUpgradeable.queueWithdraw(lpToken, amount);
     //assertEq(p12MineUpgradeable.)
   }
-  function testClaim(address lpToken)public{
+
+  function testClaim(address lpToken) public {
     p12MineUpgradeable.claim(lpToken);
-    assertGt(p12Token.balanceOf(msg.sender),0);
+    assertGt(p12Token.balanceOf(msg.sender), 0);
   }
-  function testClaimAll()public{
+
+  function testClaimAll() public {
     p12MineUpgradeable.claimAll();
-    assertGt(p12Token.balanceOf(msg.sender),0);
+    assertGt(p12Token.balanceOf(msg.sender), 0);
   }
-  function testExecuteWithdraw(address lpToken, bytes32 id)public{
+
+  function testExecuteWithdraw(address lpToken, bytes32 id) public {
     // uint256 pid = p12MineUpgradeable.getPid(lpToken);
     // address _who = p12MineUpgradeable.withdrawInfos(lpToken,id).who;
     // UserInfo memory user = p12MineUpgradeable.userInfo(pid,_who);
@@ -101,19 +115,20 @@ contract P12MineUpgradeableTest is Test{
     // uint256 amount = p12MineUpgradeable.withdrawInfos(lpToken,id).amount;
     // assertEq(p12MineUpgradeable.userInfo(pid,_who).amount,stakingBalance-amount);
   }
-  function testWithdrawAllLpTokenEmergency()public{
-    vm.prank(p12MineUpgradeable.owner()); 
+
+  function testWithdrawAllLpTokenEmergency() public {
+    vm.prank(p12MineUpgradeable.owner());
     p12MineUpgradeable.emergency();
     p12MineUpgradeable.withdrawAllLpTokenEmergency();
     //assertEq(p12MineUpgradeable.userInfo)
   }
-  function testWithdrawLpTokenEmergency(address lpToken)public{
-    // vm.prank(p12MineUpgradeable.owner()); 
+
+  function testWithdrawLpTokenEmergency(address lpToken) public {
+    // vm.prank(p12MineUpgradeable.owner());
     // p12MineUpgradeable.emergency();
     // p12MineUpgradeable.withdrawLpTokenEmergency(lpToken);
     // uint256 pid = getPid(lpToken);
     // UserInfo memory user = userInfo[pid][msg.sender];
     // assertEq(user.amount,0);
   }
-
 }
