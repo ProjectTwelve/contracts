4c2cb102a6dab9c486259ad51129c85b719b234f
Noy <59097122+noyyyy@users.noreply.github.com>
2023-07-18 14:17:29 +0800

    chore(deployments): deploy uniswap & coinfactory on butcher

--- packages/contracts/deploy/butcher/003_deploy_CoinFactory.ts
+++ packages/contracts/deploy/butcher/003_deploy_CoinFactory.ts
@@ -8,6 +8,8 @@ const func: DeployFunction = async function ({ deployments, getNamedAccounts }) 
 
   const gameCoinImpl = await get('P12GameCoin');
 
+  const posManager = await get('NonfungiblePositionManager');
+
   // Be carefully: Check whether proxy contract is initialized successfully
   await deploy('P12CoinFactoryUpgradeable', {
     from: deployer,
@@ -18,18 +20,7 @@ const func: DeployFunction = async function ({ deployments, getNamedAccounts }) 
       execute: {
         init: {
           methodName: 'initialize',
-          // v3 factory 0x1F98431c8aD98523631AE4a59f267346ea31F984
-          // https://mumbai.polygonscan.com/address/0x1F98431c8aD98523631AE4a59f267346ea31F984
-
-          // v3 pos manager 0xC36442b4a4522E871399CD717aBDD847Ab11FE88
-          // https://mumbai.polygonscan.com/address/0xC36442b4a4522E871399CD717aBDD847Ab11FE88
-          args: [
-            deployer,
-            p12Token.address,
-            '0x1F98431c8aD98523631AE4a59f267346ea31F984',
-            '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
-            gameCoinImpl.address,
-          ],
+          args: [deployer, p12Token.address, posManager.address, gameCoinImpl.address],
         },
       },
     },
