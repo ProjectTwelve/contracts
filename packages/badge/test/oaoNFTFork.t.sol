05f0c07b0b896edbc744ec3fc9b703a80937d100
Noy <59097122+noyyyy@users.noreply.github.com>
2023-09-14 19:31:54 +0800

    test: fork test oaoNFT

---
+++ packages/badge/test/oaoNFTFork.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/test.sol";
+
+import "src/oaoNFT.sol";
+
+contract OaoNFTForkTest is Test {
+    oaoNFT private _oaoNFT;
+
+    function setUp() public {
+        vm.createSelectFork("https://zetachain-athens-evm.blockpi.network/v1/rpc/public");
+        vm.rollFork(1614267);
+        _oaoNFT = oaoNFT(0x69369927AEA310b0423F475330A6Cc7302e2d060);
+    }
+
+    function testForkRunWhitelistMint() public {
+        // vm.prank();
+        // _oaoNFT.whitelistMint(
+
+        // );
+    }
+}
